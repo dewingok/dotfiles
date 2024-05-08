3d3341082b303b493b4aad4908f6b2e34b6b85ec
Nick Dewing <2049761+dewingok@users.noreply.github.com>
2024-05-07 22:02:18 -0700

    Add Python venv functions
    - Cleanup logic in aliases
    - Add vim-gnupg to nvim

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -1,3 +1,26 @@
+function activate() {
+  local env=${1:-".venv"}
+
+  if [[ ! -d "$env" ]]; then
+    echo "Virtual envirnoment '$env' not found."
+    return 1
+  fi
+
+  source "./$env/bin/activate"
+}
+
+function make_venv() {
+  local env=${1:-".venv"}
+  if [[ -d "$env" ]]; then
+    echo "Virtual environment '$env' already exists."
+    return 1
+  fi
+
+  python3 -m venv "$env"
+  source "./$env/bin/activate"
+  pip install -U pip
+}
+
 function ossl() {
   echo | openssl s_client -connect ${1}:443
 }
