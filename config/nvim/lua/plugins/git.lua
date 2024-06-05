d0bfd21f6963b1f85508337fe303bdb665f9eb93
Nick Dewing <nickdewing@gmail.com>
2024-06-05 10:28:53 -0700

    Fix gitsigns and tmux

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,12 +1,14 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
-	},
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		})
+	end,
 }
