3b77e02c5abdae7e5027a23ad9e9238d75aca88d
Nick Dewing <nickdewing@gmail.com>
2024-06-05 23:28:22 -0700

    Fixing and cleanup

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,20 +1,20 @@
 return {
-	"echasnovski/mini.nvim",
-	-- version = "*",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("mini.ai").setup({ n_lines = 500 })
+  "echasnovski/mini.nvim",
+  -- version = "*",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("mini.ai").setup({ n_lines = 500 })
 
-		require("mini.align").setup()
+    require("mini.align").setup()
 
-		require("mini.comment").setup()
+    require("mini.comment").setup()
 
-		require("mini.git").setup()
+    require("mini.git").setup()
 
-		require("mini.pairs").setup()
+    require("mini.pairs").setup()
 
-		require("mini.surround").setup()
-	end,
+    require("mini.surround").setup()
+  end,
 }
--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,24 +1,24 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	version = "*",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	cmd = "Neotree",
-	keys = {
-		{ "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
-	},
-	opts = {
-		filesystem = {
-			window = {
-				postition = "left",
-				width = 30,
-				mappings = {
-					["\\"] = "close_window",
-				},
-			},
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    { "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        postition = "left",
+        width = 30,
+        mappings = {
+          ["\\"] = "close_window",
+        },
+      },
+    },
+  },
 }
