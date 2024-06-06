baa1a5ca5e2a303102ae3b66acd2c81737d2981f
Nick Dewing <2049761+dewingok@users.noreply.github.com>
2024-06-05 23:28:22 -0700

    Fixing and cleanup

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -1,34 +1,34 @@
 return {
-	{ "nvim-tree/nvim-web-devicons" },
-	{
-		"goolord/alpha-nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.startify")
-			alpha.setup(dashboard.opts)
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("lualine").setup({
-				options = {
-					section_separators = {
-						left = "",
-						right = "",
-					},
-					component_separators = {
-						left = "",
-						right = "",
-					},
-				},
-			})
-		end,
-	},
+  { "nvim-tree/nvim-web-devicons" },
+  {
+    "goolord/alpha-nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.startify")
+      alpha.setup(dashboard.opts)
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lualine").setup({
+        options = {
+          section_separators = {
+            left = "",
+            right = "",
+          },
+          component_separators = {
+            left = "",
+            right = "",
+          },
+        },
+      })
+    end,
+  },
 }
--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,14 +1,6 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		})
-	end,
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup({})
+  end,
 }
