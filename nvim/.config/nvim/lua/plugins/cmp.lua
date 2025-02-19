ae74b8db53651cf7f109be4eae9232095090d8a6
Nick Dewing <nickdewing@gmail.com>
2025-02-19 13:52:41 -0800

    feat(nvim): restructure and update Neovim configuration
    - Modified init.lua to use new config structure
    - Added new config files for keymaps, lazy, options, and autocmds
    - Added new plugin configurations for cmp, conform, copilot, debug,
      neo-tree, telescope, treesitter, and whichkey
    - Removed old user configuration files

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,82 @@
+return {
+	-- Autocompletion
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			build = (function()
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+		},
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"zbirenbaum/copilot-cmp",
+	},
+	config = function()
+		-- See `:help cmp`
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		luasnip.config.setup({})
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			completion = { completeopt = "menu,menuone,noinsert" },
+
+			mapping = cmp.mapping.preset.insert({
+				-- Select the [n]ext or [p]revious item
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+
+				-- Scroll the documentation window [b]ack / [f]orward
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+				-- Manually trigger a completion from nvim-cmp.
+				["<C-Space>"] = cmp.mapping.complete({}),
+
+				-- Move 'right' or 'left' within snippet expansion.
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { "i", "s" }),
+				["<C-h>"] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
+			}),
+			sources = {
+				{
+					name = "lazydev",
+					group_index = 0,
+				},
+				{ name = "copilot", roup_index = 2 },
+				{ name = "nvim_lsp", group_index = 2 },
+				{ name = "luasnip" },
+				{ name = "path" },
+				{ name = "nvim_lsp_signature_help" },
+			},
+		})
+	end,
+}
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,100 @@
+return {
+	{ -- Nerd Font Icons for plugins
+		"nvim-tree/nvim-web-devicons",
+	},
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+	{ -- Better quickfix window
+		"kevinhwang91/nvim-bqf",
+	},
+	{ -- The best color scheme... ever(period).
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{ -- Statusline
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					section_separators = {
+						left = "",
+						right = "",
+					},
+					component_separators = {
+						left = "",
+						right = "",
+					},
+				},
+			})
+		end,
+	},
+	{ -- Starting page
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local startify = require("alpha.themes.startify")
+			startify.file_icons.provider = "devicons"
+			require("alpha").setup(startify.config)
+		end,
+	},
+	{ -- Seemless navigation between Nvim and Tmux.
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
+			nvim_tmux_nav.setup({
+				disable_when_zoomed = true,
+			})
+			vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+		end,
+	},
+	{ -- Powerful comments
+		"numToStr/Comment.nvim",
+	},
+	{ -- Highlight todo, notes, etc in comments
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.ai").setup({ n_lines = 500 })
+		end,
+	},
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
+	{ -- Autopairs
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+}
